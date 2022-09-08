4d8a4b5469c8ff3f942056d9abba5abe65154c0b
Pedro Palugan <88800549+pedropalugan@users.noreply.github.com>
2022-09-07 22:07:05 -0300

    Add files via upload

---
+++ front/App.js
@@ -0,0 +1,43 @@
+import { StyleSheet, Text, SafeAreaView, Button, TextInput } from 'react-native';
+import { launchImageLibrary, ImagePicker } from 'react-native-image-picker';
+import { useState } from 'react';
+import Axios from 'axios'
+
+export default function App() {
+
+  const [img, setImg] = useState(null)
+
+
+
+
+  const upload = () => {
+    console.log(img)
+
+    const form = new FormData()
+
+    form.append('image', img)
+
+    fetch('http://localhost:3000/uploadMealPic', {
+      method : 'POST',
+      body : form
+    }).then((response) => console.log(response))
+    .catch((error) => console.log(error))
+  }
+
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Button title="Submit" onPress={upload}/>
+      <input type='file' onChange={e => setImg(e.target.files[0])}/>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
